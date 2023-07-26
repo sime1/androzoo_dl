f98f27797d130971aa66d3712083cd5a2149a0b8
sime1 <sime.pizzi@gmail.com>
2023-07-26 16:20:49 +0200

    add README + option to just output a filtered csv

---
+++ README.md
@@ -0,0 +1,17 @@
+# Androzoo Downloader
+
+This program can be used to download the latest versions of a list of apks
+from the androzoo dataset. You need to provide the apk list csv.
+
+```
+Usage: az_dl [OPTIONS] --packages <PACKAGES> --csv <CSV> --output <OUTPUT>
+
+Options:
+  -a, --api-key <API_KEY>
+  -p, --packages <PACKAGES>
+  -c, --csv <CSV>
+  -o, --output <OUTPUT>
+  -d, --download
+  -h, --help                 Print help
+  -V, --version              Print version
+```
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::{fs::File, error::Error, collections::HashMap, io::Write};
 use serde::{Deserialize, Serialize};
 use clap::Parser;
 use indicatif::{ProgressBar, ProgressStyle};
+use csv;
 
 
 #[derive(Parser, Debug)]
@@ -9,7 +10,7 @@ use indicatif::{ProgressBar, ProgressStyle};
 struct Args {
     // Androzoo API key
     #[arg(short, long)]
-    api_key: String,
+    api_key: Option<String>,
 
     // yaml file containing the list of packages to download
     #[arg(short, long)]
@@ -22,6 +23,10 @@ struct Args {
     // path to folder in which the files will be saved
     #[arg(short, long)]
     output: String,
+
+    // whether to download the apks or not. Default is false.
+    #[arg(short, long)]
+    download: bool,
 }
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
@@ -34,6 +39,15 @@ struct Record {
 #[tokio::main]
 async fn main() -> Result<(),  Box<dyn Error>> {
     let args = Args::parse();
+
+    if args.download {
+        if let None = &args.api_key {
+            println!("Error! need to provide an API key to download");
+            let err = clap::Error::new(clap::error::ErrorKind::MissingRequiredArgument);
+            return Err(From::from(err));
+        } 
+    }
+
     let pkg_file = File::open(args.packages)?;
     let pkgs: Vec<String> = serde_yaml::from_reader(pkg_file)?;
     let csv_file = File::open(args.csv)?;
@@ -65,17 +79,25 @@ async fn main() -> Result<(),  Box<dyn Error>> {
         }
     }
 
+    let api_key = args.api_key.unwrap_or("".into());
     let apps: Vec<_> = apps.values().collect();
     bar.set_length(apps.len() as u64);
     std::fs::create_dir_all(&args.output)?;
+    let out_csv_filename = format!("{}/filtered.csv", &args.output);
+    let out_csv = File::create(&out_csv_filename)?;
+    let mut out_csv = csv::WriterBuilder::new().has_headers(true).from_writer(out_csv);
     for app in apps {
+        out_csv.serialize(&app)?;
         bar.set_message(app.pkg_name.clone());
-        let filename = format!("{}/{}.apk", &args.output, &app.pkg_name);
-        let mut out = File::create(filename)?;
-        let url = format!("https://androzoo.uni.lu/api/download?apikey={}&sha256={}", &args.api_key, &app.sha256);
-        let res = reqwest::get(url).await?.bytes().await?;
-        out.write_all(&res[..])?;
-        bar.inc(1)
+        if args.download {
+            let filename = format!("{}/{}.apk", &args.output, &app.pkg_name);
+            let mut out = File::create(filename)?;
+            let url = format!("https://androzoo.uni.lu/api/download?apikey={}&sha256={}", &api_key, &app.sha256);
+            let res = reqwest::get(url).await?.bytes().await?;
+            out.write_all(&res[..])?;
+        }
+        bar.inc(1);
     }
+    out_csv.flush()?;
     Ok(())
 }
